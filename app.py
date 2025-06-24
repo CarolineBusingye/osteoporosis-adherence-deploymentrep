6e07cd04c52ac71345a238bb26e9eae50bfcf598
Busingye Caroline <107537463+CarolineBusingye@users.noreply.github.com>
2025-06-24 13:46:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Load your trained model
-with open('model.pkl', 'rb') as f:
+with open('scaler.pkl', 'rb') as f:
     model = pickle.load(f)
 
 @app.route('/')
