53d937af64ea12508072526033cf115099a356f6
Busingye Caroline <107537463+CarolineBusingye@users.noreply.github.com>
2025-06-24 15:41:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 from flask import Flask, request, jsonify, render_template
 import xgboost as xgb
 import numpy as np
-import pickle
+import joblib
+
 
 app = Flask(__name__)
 
-# Load the saved XGBoost model
-with open("best_xgboost_model (2).pkl", "rb") as file:
-    model = pickle.load(file)
-
-# Load the saved scaler
-with open("scaler (3).pkl", "rb") as scaler_file:
-    scaler = pickle.load(scaler_file)
+scaler = joblib.load("scaler (3).pkl")
+model = joblib.load("best_xgboost_model (2).pkl")
 
 @app.route('/')
 def home():
